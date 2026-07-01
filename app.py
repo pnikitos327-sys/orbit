bbb93846e980f20a54882ae91888e7b2103221ac
pnikitos327-sys <owwwwkom@gmail.com>
2026-07-01 17:44:16 +0300

    create table

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///links.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+class link(db.Model):
+    id = db.сolumn(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(500), nullable = False)
+    short_code = db.Column(db.String(20), unique=True, nullable = False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    clicks = db.column(db.Integer, default=0)
+
 
 @app.route('/')
 def home():
