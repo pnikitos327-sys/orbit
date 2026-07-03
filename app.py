7e2b95c89e43e12ebda7714674ba6694a789d0d0
pnikitos327-sys <owwwwkom@gmail.com>
2026-07-03 18:31:11 +0300

    new change in app.py

--- app.py
+++ app.py
@@ -9,6 +9,23 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///links.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+def generate_short_code():
+    chars = string.ascii_letters + string.digits
+    return ''.join(random.choice(chars) for _ in range(6))
+
+
+app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.form == 'POST':
+        url = request.method['url']
+        code = generate_short_code()
+        new_link = Link(original_url=url, short_code=code)
+        db.session.add(new_link)
+        db.session.commit()
+        return redirect(url_for('home'))
+        links = Link.query.order_by(Link.created_at.desc()).all()
+        return render_template('index.html', links=links)
+
 class Link(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original_url = db.Column(db.String(500), nullable=False)
